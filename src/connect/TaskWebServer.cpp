58b01a6fc19b9ee3df44d6550d8feef2a204cf68
huynh0210 <phanhuynhcse0210@gmail.com>
2025-06-30 21:58:52 +0700

    updateRGB

--- src/connect/TaskWebServer.cpp
+++ src/connect/TaskWebServer.cpp
@@ -3,7 +3,7 @@
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-void notifyAllClients() {
+String createStatusJSON() {
   String json = "{";
   json += "\"type\":\"sensor\",";
   json += "\"Temp\":" + String(dht20.getTemperature()) + ",";
@@ -12,29 +12,61 @@ void notifyAllClients() {
   json += "\"Distance\":" + String(test()) + ",";
   json += "\"Lux\":" + String(getValueLux()) + ",";
   json += "\"A\":\"" + singleLedState + "\",";
-  json += "\"C\":\"" + LedRGB + "\"";
+  json += "\"C\":\"" + LedRGB + "\",";
+  json += "\"rgbMode\":\"" + rgbMode + "\",";
+  json += "\"customR\":" + String(customRed) + ",";
+  json += "\"customG\":" + String(customGreen) + ",";
+  json += "\"customB\":" + String(customBlue);
   json += "}";
-  ws.textAll(json);
+  return json;
 }
 
-void handleWebSocketMessage(AsyncWebSocketClient *client, String msg) {
+void notifyAllClients() {
+  ws.textAll(createStatusJSON());
+}
+
+void handleLEDAControl(String msg) {
   if (msg.startsWith("toggleLED:")) {
     char led = msg.charAt(10);
     String state = msg.substring(12);
     if (led == 'A') {
       setSingleLedState(state);
-    } else if (led == 'C') {
+      notifyAllClients();
+    }
+  }
+}
+
+void handleLEDCControl(String msg) {
+  if (msg.startsWith("toggleLED:")) {
+    char led = msg.charAt(10);
+    String state = msg.substring(12);
+    if (led == 'C') {
       digitalWrite(PIN_NEO_PIXEL, state == "on" ? LOW : HIGH);
       LedRGB = state;
+      notifyAllClients();
     }
   } else if (msg.startsWith("rgbMode:")) {
-    rgbMode = msg.substring(8);
+    String mode = msg.substring(8);
+    setRGBMode(mode);
+    notifyAllClients();
   } else if (msg.startsWith("setColor:")) {
     int r, g, b;
     sscanf(msg.c_str(), "setColor:%d,%d,%d", &r, &g, &b);
-    customRed = r;
-    customGreen = g;
-    customBlue = b;
+    setCustomColor(r, g, b);
+    notifyAllClients();
+  }
+}
+
+void handleWebSocketMessage(AsyncWebSocketClient *client, String msg) {
+  if (msg.startsWith("toggleLED:")) {
+    char led = msg.charAt(10);
+    if (led == 'A') {
+      handleLEDAControl(msg);
+    } else if (led == 'C') {
+      handleLEDCControl(msg);
+    }
+  } else if (msg.startsWith("rgbMode:") || msg.startsWith("setColor:")) {
+    handleLEDCControl(msg);
   }
 }
 
@@ -42,7 +74,7 @@ void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
                       AwsEventType type, void *arg, uint8_t *data, size_t len) {
   if (type == WS_EVT_CONNECT) {
     Serial.println("Client connected");
-    client->text("connected");
+    client->text(createStatusJSON());
   } else if (type == WS_EVT_DATA) {
     String msg = "";
     for (size_t i = 0; i < len; i++) msg += (char)data[i];
